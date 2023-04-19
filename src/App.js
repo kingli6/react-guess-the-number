5ddc2fe95cb6a6e2263fe2b5ea4226d3bc21cad4
Joanthan Fisher <jonathan.fshr@gmail.com>
2023-04-19 12:21:42 +0200

    setting up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+// import { Game } from "./Game";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {
+        <div>
+          <Component />
+          <Gamelogic />
+        </div>
+      }
+    </div>
+  );
 }
 
+function Component() {
+  return (
+    <div>
+      <h2>hello</h2>
+    </div>
+  );
+}
+
+const Gamelogic = () => {
+  return (
+    <div>
+      <p>Some text describing an idea</p>
+    </div>
+  );
+};
 export default App;
---
+++ src/Game.js
@@ -0,0 +1,8 @@
+function Game() {
+  return (
+    <div>
+      <h2>Hi</h2>
+    </div>
+  );
+}
+export default Game;

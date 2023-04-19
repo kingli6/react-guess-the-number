9211a74815519a88c8233394a66d37e497099ff0
Joanthan Fisher <jonathan.fshr@gmail.com>
2023-04-19 16:31:50 +0200

    guess logic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,76 @@
+import React, { useState } from "react";
 import "./App.css";
-// import { Game } from "./Game";
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       {
         <div>
-          <Component />
           <Gamelogic />
         </div>
       }
     </div>
   );
 }
-
-function Component() {
-  return (
-    <div>
-      <h2>hello</h2>
-    </div>
-  );
-}
+/* So far....
+with button click, we choose a random number. */
 
 const Gamelogic = () => {
-  return (
-    <div>
-      <p>Some text describing an idea</p>
-    </div>
-  );
+  const [gameStarted, setGameStarted] = useState(false);
+  const [message, setMessage] = useState(false);
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState(0);
+
+  const difficulty = [
+    { value: "level1", label: "Level 1" },
+    { value: "level2", label: "Level 2" },
+    { value: "level3", label: "Level 3" },
+  ];
+  //functions/////////////////////////////
+  const start = () => {
+    setRandomNumber(Math.ceil(Math.random() * 10));
+    setGameStarted(true);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault(); //stops from submitting the form (default action)
+    const checkInput = +userAnswer >= 0; //validate userinput
+    if (!checkInput) return;
+    if (+userAnswer === +randomNumber) {
+      setMessage("Correct!");
+      setGameStarted(false);
+    } else if (+userAnswer < +randomNumber) {
+      setMessage("too low");
+    } else {
+      setMessage("too high");
+    }
+  };
+  //rendering/////////////////////////////
+  if (gameStarted) {
+    return (
+      <div>
+        <form onSubmit={submitHandler}>
+          <div>
+            <label>answer</label>
+            <input
+              value={userAnswer}
+              onChange={(e) => setUserAnswer(e.target.value)}
+            />
+          </div>
+          <button type="submitHandler">Check</button>
+        </form>
+        <p>random number: {randomNumber}</p>
+        <p>{message}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button type="button" onClick={start}>
+          Start
+        </button>
+
+        <p>{message}</p>
+      </div>
+    );
+  }
 };
-export default App;
---
+++ src/Button.js
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+
+// export default function Button() {
+//   const [diffValue, setDiffValue] = useState("level-1");
+//   const difficultyHandler = (e) => {
+//     setDiffValue(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <select value={value} onChange={difficultyHandler}>
+//         <option value="level-1">Level 1</option>
+//         <option value="level-2">Level 2</option>
+//         <option value="level-3">Level 3</option>
+//       </select>
+//       <button type="button">choose</button>
+//     </div>
+//   );
+// }
